dedb196add803f5479e533789f316ff909b71cdc
OlexandrBilyk <147237948+OlexandrBilyk@users.noreply.github.com>
2025-07-01 20:57:32 +0000

    Deploying to gh-pages from @ OlexandrBilyk/CalculatorOnReact@72e88919f4bc6e87cd4dd78d477a6512ede4ed24 🚀

--- src/components/CalcButton.jsx
+++
@@ -1,5 +0,0 @@
-export default function CalcButton({ children, onClick, className}) {
-    return (
-        <button className={className} onClick={onClick}>{children}</button>
-    )
-}
--- src/components/CalcDisplay.jsx
+++
@@ -1,5 +0,0 @@
-export default function CalcDisplay({ value }) {
-    return (
-        <section className="calc__display">{value}</section>
-    )
-}
